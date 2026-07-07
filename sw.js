24a43a37283300344362bcc451fc4ad8af3b9f0c
AnestDar <your_pain7@mail.ru>
2026-07-07 18:53:44 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'carotid-vol-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => cachedResponse || fetch(event.request))
+  );
+});
